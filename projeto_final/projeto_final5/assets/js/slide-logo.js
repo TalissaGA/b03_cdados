c0e6afb739717bf7de54998b09554ec5172977d7
TalissaGA <56507707+TalissaGA@users.noreply.github.com>
2020-06-19 00:44:43 -0300

    apagando uma pasta amais

--- projeto_final/projeto_final5/assets/js/slide-logo.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let params = coDesExtract()
-    let value = params['key']
-    let value2 = document.querySelector(".slides-icon-slide")
-    value2.src="assets/img/" + value + ".jpeg"
-  })
